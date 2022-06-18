b66090a8c92e80692b50b94cf1ad0f07314777f8
leonardomartelli <leonardo@martellioliveira.me>
2022-06-18 02:32:07 -0300

    plese be right

--- SynAna/Syntactic.cs
+++ SynAna/Syntactic.cs
@@ -4,25 +4,82 @@ using SynAna.LexAna;
 
 namespace SynAna
 {
-	public class Syntactic
-	{
+    public class Syntactic
+    {
         private Token _currentToken;
 
         private readonly IEnumerator<Token> _lexicalResultEnumerator;
 
         public Syntactic(IEnumerable<Token> lexicalResult)
-		{
+        {
             _lexicalResultEnumerator = lexicalResult.GetEnumerator();
         }
 
-        private void ReadToken()
+        void ReadToken() =>
+            _currentToken = _lexicalResultEnumerator.MoveNext()
+                    ? _lexicalResultEnumerator.Current
+                    : Token.EOF;
+
+        // external_declaration -> function_defintion | declaration
+        bool external_declaration()
         {
-   
-            if(_lexicalResultEnumerator.MoveNext())
-            {
-                _currentToken = _lexicalResultEnumerator.Current;
-            }
+            if (function_definition())
+                return true;
+
+            else if (declaration())
+                return true;
+
+            return false;
         }
-	}
+
+        bool function_definition()
+        {
+            if (declaration_specifiers())
+            {
+                ReadToken();
+
+                if (declarator())
+                {
+                    ReadToken();
+
+                    if (declaration_list())
+                    {
+                        ReadToken();
+
+                        if (compound_statement())
+                            return true;
+                    }
+                    else if (compound_statement())
+                        return true;
+                }
+
+            }
+            else if (declarator())
+            {
+                ReadToken();
+
+                if (declaration_list())
+                {
+                    ReadToken();
+
+                    if (compound_statement())
+                        return true;
+                }
+                else if (compound_statement())
+                    return true;
+            }
+            return false;
+        }
+
+        bool declaration_specifiers()
+        {
+
+        }
+
+        bool declaration()
+        {
+
+        }
+    }
 }
 
